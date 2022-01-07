a0df24e3e3a1d923f3494ba68cdfa0761a73a0f0
Igor Silva <carlos.igor@original.com.br>
2022-01-07 15:32:29 -0300

    Update GitHubProjectProjectRepository.kt

--- app/src/main/java/com/bankuish/challenge/data/GitHubProjectProjectRepository.kt
+++ app/src/main/java/com/bankuish/challenge/data/GitHubProjectProjectRepository.kt
@@ -1,7 +1,5 @@
 package com.bankuish.challenge.data
 
-import retrofit2.http.Query
-
 class GitHubProjectProjectRepository(private val retrofitService: GitHubService) :
     IGitHubProjectRepository {
     override suspend fun getKotlinRepositories(
