fae44d01c96d938b94c170dccba3722b215e9043
Igor Silva <carlos.igor@original.com.br>
2022-01-09 18:54:33 -0300

    test adjustments

--- app/src/main/java/com/bankuish/challenge/data/GitHubProjectRepository.kt
+++ app/src/main/java/com/bankuish/challenge/data/GitHubProjectRepository.kt
@@ -1,16 +1,12 @@
 package com.bankuish.challenge.data
 
-import kotlinx.coroutines.runBlocking
 import retrofit2.Response
 
- class GitHubProjectRepository(private val retrofitService: GitHubService) :
+class GitHubProjectRepository(private val retrofitService: GitHubService) :
     IGitHubProjectRepository {
     override suspend fun getKotlinRepositories(
         language: String,
         resultsPerPage: String,
         pageNumber: String
-    ): Response<GitHubProjectResponse> {
-        return retrofitService.getGithubProjects(language, resultsPerPage, pageNumber)
-
-    }
+    ): Response<GitHubProjectResponse> = retrofitService.getGithubProjects(language, resultsPerPage, pageNumber)
 }
--- app/src/test/java/com/bankuish/challenge/GitHubProjectRepositoryTest.kt
+++ app/src/test/java/com/bankuish/challenge/GitHubProjectRepositoryTest.kt
@@ -1,10 +1,11 @@
-package com.bankuish.challenge.data
+package com.bankuish.challenge
 
+import com.bankuish.challenge.data.GitHubProjectRepository
+import com.bankuish.challenge.data.GitHubProjectResponse
+import com.bankuish.challenge.data.GitHubService
+import com.bankuish.challenge.data.IGitHubProjectRepository
 import com.bankuish.challenge.di.getServiceInstance
-import com.bankuish.challenge.domain.GitHubProjectUseCase
-import org.junit.Assert.*
 
-import junit.framework.TestCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
@@ -16,7 +17,6 @@ import org.junit.Before
 import org.junit.Test
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
-import org.koin.java.KoinJavaComponent.inject
 import org.koin.test.KoinTest
 import org.koin.test.inject
 import org.mockito.Mockito
@@ -25,19 +25,8 @@ import retrofit2.Response
 
 class GitHubProjectRepositoryTest : KoinTest {
     private val sampleModule = module {
+        single<IGitHubProjectRepository> { GitHubProjectRepository(get()) }
         single { getServiceInstance() }
-    }
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-
-    @Before
-    fun setUp() {
-        Dispatchers.setMain(mainThreadSurrogate)
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
-        mainThreadSurrogate.close()
     }
 
     init {
@@ -45,19 +34,20 @@ class GitHubProjectRepositoryTest : KoinTest {
     }
 
     private val service: GitHubService = mock()
-    private val gitHubProjectRepository: GitHubProjectRepository by inject()
+    private val gitHubProjectRepository: IGitHubProjectRepository by inject()
 
 
     @Test
     fun `when get projects and have success response`() {
-        val respGit: GitHubProjectResponse = mock()
-        val resp: Response<GitHubProjectResponse> = Response.success(200, respGit)
         runBlocking {
+            val respGit = GitHubProjectResponse()
+            val resp: Response<GitHubProjectResponse> = Response.success(200, respGit)
             Mockito.`when`(service.getGithubProjects("", "", "")).thenReturn(
                 resp
             )
             val response = gitHubProjectRepository.getKotlinRepositories("", "", "")
-            Mockito.verify(response.isSuccessful, Mockito.times(1))
+            assert(response.isSuccessful)
+            assert(response.code() == 200)
         }
     }
 
@@ -70,7 +60,8 @@ class GitHubProjectRepositoryTest : KoinTest {
                 resp
             )
             val response = gitHubProjectRepository.getKotlinRepositories("", "", "")
-            Mockito.verify(!response.isSuccessful, Mockito.times(1))
+            assert(!response.isSuccessful)
+            assert(response.code() == 500)
         }
     }
 }
